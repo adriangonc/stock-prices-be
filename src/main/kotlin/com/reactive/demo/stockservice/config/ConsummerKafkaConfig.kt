a1bc35b83560337e4a88a309839e8c00b84ab45c
Adriano Gonçalves de Souza <adriangonc@yahoo.com.br>
2023-01-09 14:41:02 -0300

    Criação de consumidor para o tópico employee

---
+++ src/main/kotlin/com/reactive/demo/stockservice/config/ConsummerKafkaConfig.kt
@@ -0,0 +1,40 @@
+package com.reactive.demo.stockservice.config
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.kafka.support.serializer.JsonSerializer
+
+@EnableKafka
+@Configuration
+class ConsumerKafkaConfig(
+    @Autowired
+    private val kafkaProperties: KafkaProperties
+) {
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, Any> {
+        val configProps: MutableMap<String, Any> = HashMap()
+        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaProperties.bootstrapServers
+        configProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        configProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
+        return DefaultKafkaConsumerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        var factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+        factory.consumerFactory = consumerFactory()
+        return factory
+    }
+
+}
